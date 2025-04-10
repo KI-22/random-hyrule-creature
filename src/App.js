b4c6b7957136edbbdbb05f8d10118bfd2a6f5a93
Kate <katebancroft@yahoo.co.uk>
2025-04-10 16:26:37 +0100

    Not fully working, but the files run now at least

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from 'react'
+import Temp from './Temp'
+
+function App() {
+    // <Temp />
+    return <>
+        <Temp />
+    </>
+}
+
+export default App
---
+++ src/Temp.jsx
@@ -0,0 +1,10 @@
+const Temp = () => {
+    const returnTest = "test return :)";
+    return <>
+        <div className="container">
+            <h3>returnTest</h3>
+        </div>
+    </>
+}
+
+export default Temp;
